aa912a2b64a85841b4c16e0e2399d522c220051e
AbdulRahmanPro <progammerabood@gmail.com>
2023-10-17 20:37:09 +0300

    upload

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,19 +1,20 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './assets/CSS/index.css'
-import 'semantic-ui-css/semantic.min.css'
-import { Provider } from 'react-redux'
-import AccountStore from  './redux/store.jsx'
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './assets/CSS/index.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Provider } from 'react-redux';
+import AccountStore from './redux/store.jsx';
 import GlobalStyle from './assets/CSS/GlobalStyles';
 
 // الملف الرئيسي الذي يشغل الموقع
 const rootElement = document.getElementById('root');
 if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
+  ReactDOM.render(
     <Provider store={AccountStore}>
       <GlobalStyle />
       <App />
-    </Provider>
+    </Provider>,
+    rootElement
   );
 } else {
   console.error("Element with id 'root' not found");
